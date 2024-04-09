ec8d01f801dbef14302eb850e290da64ae7010dc
Dayasagar543 <dayasagar.543@gmail.com>
2024-04-09 15:01:49 +0530

    commiting the files on object creation, method overloading,array and multi dimensional array

--- oops/Calculator.java
+++ oops/Calculator.java
@@ -10,9 +10,35 @@ public class Calculator {
         return a + b;
     }
 
+    // method overloading:
+    // we can create multiple methods of same kind but the jvm will take the
+    // method name or adopts its self to the number of paratmeters its given to it
+    // and calls the method if the methods are of same name..
+    // here is an example below
+
+    public int add(int a, int b, int c) {
+        System.out.println("this is the solution from 3 parameters additon");
+        return a + b + c;
+    }
+
+    public int subtraction(int a, int b) {
+        System.out.println("the difference of the two numbers from the calcualtor class is");
+        return a - b;
+    }
+
     public int mul(int a, int b) {
         System.out.println("the product of the two numbers from the calcualtor class is");
         return a * b;
+    }
+
+    public int div(int a, int b) {
+        System.out.println("the quotient of the two numbers from the calcualtor class is \t");
+        return a / b;
+    }
+
+    public int remainder(int a, int b) {
+        System.out.println("the remainder of the two numbers from the calcualtor class is ");
+        return a % b;
     }
 
 }
--- oops/Main.java
+++ oops/Main.java
@@ -2,12 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // calling the class
         Calculator cal = new Calculator();
-        int result2 = cal.add(34, 20);
+
+        // calling the addd method from the calculator class using the object cal
+        int result1 = cal.add(34, 20);
+        System.out.println(result1);
+
+        int result1_1 = cal.add(4, 5, 7);
+        System.out.println(result1_1);
+
+        int result2 = cal.subtraction(2, 4);
         System.out.println(result2);
 
-        int mul_result = cal.mul(34, 21);
-        System.out.println(mul_result);
+        int result3 = cal.mul(34, 21);
+        System.out.println(result3);
+
+        int result4 = cal.div(4, 2);
+        System.out.println(result4);
+
+        int result5 = cal.remainder(4, 2);
+        System.out.println(result5);
 
         int resutl = add(2, 3);
         System.out.println("The result from the main static method is   " + resutl);
