965be0089f23c6c78980c3cfa667a82a8333ab0a
Dayasagar543 <95306789+Dayasagar543@users.noreply.github.com>
2024-07-06 22:39:58 +0530

    Delete oops directory

--- oops/Calculator/Method_overriding.java
+++
@@ -1,8 +0,0 @@
-
-public class Method_overriding {
-    public static void main(String[] args) {
-        Advanced_Calculator adc = new Advanced_Calculator();
-        System.out.println(adc.add(2, 3));
-        System.out.println(adc.add(5, 43));
-    }
-}
