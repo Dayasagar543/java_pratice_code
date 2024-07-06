9db2868b1e9b4ca0a8b0e5a51a1e279418f179d0
Dayasagar543 <95306789+Dayasagar543@users.noreply.github.com>
2024-07-06 22:39:41 +0530

    Delete course directory

--- course/Ternary_operator.java
+++
@@ -1,8 +0,0 @@
-public class Ternary_operator {
-    public static void main(String[] args) {
-        int divisor = 5;
-        int dividend = 100;
-        int result= dividend % divisor == 0 ? dividend/divisor: dividend/divisor;
-        System.out.println(result);
-    }
-}
