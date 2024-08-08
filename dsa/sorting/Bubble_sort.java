9d397dad1059bed06e9bfed384cf7cb7e01f5a11
Dayasagar543 <dayasagar.543@gmail.com>
2024-08-08 19:56:47 +0530

    added the bubble sort file and modified the main file

--- dsa/sorting/Bubble_sort.java
+++ dsa/sorting/Bubble_sort.java
@@ -5,12 +5,13 @@ public class Bubble_sort {
 
     public void sort_bubble(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
+            for (int j = 1; j < array.length - i; j++) {
+                // if (array[j] > array[j + 1]) {
+                if (array[j-1] > array[j]) {
                     // int temp = array[j];
                     // array[j] = array[j + 1];
                     // array[j + 1] = temp;
-                    sw.swap(array, j, j+1);
+                    sw.swap(array, j, j-1);
                 }
             }
             // ao.print_array(array);
